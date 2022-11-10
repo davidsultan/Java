22ccefa242a556760db3b0e8619bd8ea5e55647f
sonersg <sonersg@outlook.com>
2022-11-10 12:37:04 +0300

    Add files via upload

---
+++ coderbyte/DivisionByThreeFive/src/main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static String StringChallenge(int num) {
+        // code goes here
+        String result = "";
+        for(int i=1; i<=num; i++){
+
+            if(i%3 == 0 && i%5 == 0){
+                result += "FizzBuzz";
+            }else if(i%3 == 0){
+                result += "Fizz";
+            }else if(i%5 == 0){
+                result += "Buzz";
+            }else {
+                result += String.valueOf(i);
+            }
+            result +=" ";
+
+        }
+        return result;
+    }
+
+    public static void main (String[] args) {
+        // keep this function call here
+        Scanner s = new Scanner(System.in);
+        System.out.print(StringChallenge(s.nextInt()));
+    }
+
+}
